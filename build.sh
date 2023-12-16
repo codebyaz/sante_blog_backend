61c02c02e2986dde130aa3e8de2095f836d53c4b
AZ <az@codebyaz.co>
2023-12-16 18:42:30 +0000

    env: improve build

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+source .env
+
 # install requirements
 echo '### installing requirements to allow running django manage ###'
 pip install -r requirements.txt
@@ -5,3 +7,4 @@ pip install -r requirements.txt
 # collect static files
 echo '### collecting static files ###'
 python3 manage.py collectstatic
+python manage.py createsuperuser --username $ADMIN_USER --password $ADMIN_PASSWORD
