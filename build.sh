f210a1f5820dae3c97a3599ff96ba10ba3c6139e
AZ <az@codebyaz.co>
2023-12-16 18:23:29 +0000

    env: improve build

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
+# install requirements
+echo '### installing requirements to allow running django manage ###'
+pip install -r requirements.txt
+
 # collect static files
+echo '### collecting static files ###'
 python3 manage.py collectstatic
