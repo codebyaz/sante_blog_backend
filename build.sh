97b92f27f3d7dc94d13d70074767c3ef9c6b337c
AZ <az@codebyaz.co>
2023-12-17 16:39:13 +0000

    env: add venv and migrations to build

--- build.sh
+++ build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+echo '### creating venv ###'
+python3 -m venv env
+
+echo '### activate venv ###'
+source env/bin/activate
+
 echo '### installing requirements to allow running django manage ###'
 pip install -r requirements.txt
 
 echo '### collecting static files ###'
 python3 manage.py collectstatic
+
+echo '### running migrations ###'
+python3 manage.py migrate
 
 echo '### create superuser ###'
 python3 manage.py createsuperuser --noinput
